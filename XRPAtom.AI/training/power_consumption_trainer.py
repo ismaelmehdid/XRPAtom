c5a0c917dc65526e718208872ca0a84e060e6289
Jan Matter <jan.matter@outlook.com>
2025-04-06 10:24:35 +0200

    final commit ai

--- XRPAtom.AI/training/power_consumption_trainer.py
+++ XRPAtom.AI/training/power_consumption_trainer.py
@@ -72,8 +72,8 @@ class PowerConsumptionTrainer:
     def train(self, epochs):
         val_results = {}
         for epoch in range(epochs):
+            train_loss = self.train_one_epoch()
             val_results = self._validator.validate()
             print(f"Epoch {epoch}/{epochs}, Val Loss: {val_results["error"]}")
-            train_loss = self.train_one_epoch()
             print(f"Epoch {epoch}/{epochs}, Train Loss: {train_loss['hourly_model_loss']}, {train_loss['daily_model_loss']}")
         return val_results
