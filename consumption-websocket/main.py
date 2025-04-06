f259775716ff2fb41d01bae2be2f2ee09c831c3c
andrii-bluco <andrii@bluco.ai>
2025-04-06 11:15:58 +0200

    middleware

--- consumption-websocket/main.py
+++ consumption-websocket/main.py
@@ -3,8 +3,19 @@ from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from websocket_manager import WebSocketManager
 from consumption_simulation import simulate_consumption
 from meross_device_manager import MerossDeviceManager
+from fastapi.middleware.cors import CORSMiddleware
+
+
 app = FastAPI(docs_url="/docs", redoc_url=None, title="Real-time consumption websocket", version="1.0.0")
 ws_manager = WebSocketManager()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 device_manager = MerossDeviceManager(
     email="dresyv@gmail.com",
